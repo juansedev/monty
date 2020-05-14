f0f5576b1b0815a472ba251f36a6d6ccfe1874ba
crispthoalex <alex877@gmail.com>
2020-05-14 20:25:58 +0000

    Add opcode nop

--- opcode_functions.c
+++ opcode_functions.c
@@ -94,3 +94,15 @@ void fn_add(stack_t **stack, unsigned int line_number)
 
         print_dlistint(*stack);
 }
+/**
+ * fn_nop - doesnt do anything
+ * @stack: pointer to head of stack
+ * @line_number: number of the line in the bytecode file
+ * Return: number of nodes in the linked list, 0 if empty
+ */
+void fn_nop(stack_t **stack, unsigned int line_number)
+{
+        (void)line_number;
+
+        print_dlistint(*stack);
+}
