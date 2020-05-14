4da6bea53378c80c723099c81bf8988a9b19333b
Juan Sebastian Llano <juanllano93@gmail.com>
2020-05-14 03:25:49 +0000

    add task 0 with error message

--- list_funcionts.c
+++ list_funcionts.c
@@ -36,7 +36,10 @@ stack_t *add_dnodeint(stack_t **head, const int n)
 
 	new_node = malloc(sizeof(stack_t));
 	if (new_node == NULL)
-		return (NULL);
+	{
+		printf("Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
 
 	new_node->n = n;
 
