d48fb7a6dd8f7647b735c32f684f8a134625c84e
crispthoalex <alex877@gmail.com>
2020-05-15 06:27:54 +0000

    evaluatefclose()

--- opcode_3.c
+++ opcode_3.c
@@ -43,7 +43,7 @@ void fn_pchar(stack_t **stack, unsigned int line_number)
 		print_charlist(*stack);
 	else
 	{
-		fclose(line_global.file_i);
+		/*fclose(line_global.file_i);*/
 		dprintf(STDERR_FILENO, "L%d: can't pchar, stack empty\n",
 				line_global.number_line);
 		exit(EXIT_FAILURE);
